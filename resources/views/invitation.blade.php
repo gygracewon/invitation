c730bdc598edeb2fef220a3866042572ac4f9519
ga young won <845835774@44381-501.auckland.natcoll.ac.nz>
2016-11-18 13:55:00 +1300

    first commit

---
+++ resources/views/invitation.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+vhcghfghfgh
+</body>
+</html>
